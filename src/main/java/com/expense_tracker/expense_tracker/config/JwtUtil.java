68e84e81492bdb315661759524e05dfd803e5c6b
Manish Kumar <manish.kumar@sarasanalytics.com>
2025-04-27 14:05:35 +0530

    defined secret key

--- src/main/java/com/expense_tracker/expense_tracker/config/JwtUtil.java
+++ src/main/java/com/expense_tracker/expense_tracker/config/JwtUtil.java
@@ -2,23 +2,34 @@ package com.expense_tracker.expense_tracker.config;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
 
-    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    @Value("${jwt.secret}")
+    private String secret;
+
     private final long expirationMillis = 24 * 60 * 60 * 1000; // 1 day
+
+    public SecretKey getSecretKey() {
+        byte[] decodedKey = Base64.getDecoder().decode(secret);
+        return Keys.hmacShaKeyFor(decodedKey);
+    }
+
 
     public String generateToken(String email) {
         return Jwts.builder()
                 .setSubject(email)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expirationMillis))
-                .signWith(key)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
@@ -37,7 +48,7 @@ public class JwtUtil {
 
     private Claims getClaims(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(key)
+                .setSigningKey(getSecretKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
