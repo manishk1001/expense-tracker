114d8e8f22c2515432c240d305ddff12518b9ce1
Manish Kumar <manish.kumar@sarasanalytics.com>
2025-05-17 05:49:40 +0530

    removed hardcoded kafka server

--- src/main/java/com/expense_tracker/expense_tracker/config/KafkaConfig.java
+++ src/main/java/com/expense_tracker/expense_tracker/config/KafkaConfig.java
@@ -3,6 +3,7 @@ package com.expense_tracker.expense_tracker.config;
 import com.expense_tracker.expense_tracker.dto.PasswordResetEvent;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.*;
@@ -13,11 +14,13 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafka_server;
 
     @Bean
     public ProducerFactory<String, PasswordResetEvent> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // update your broker address
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafka_server); // update your broker address
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
